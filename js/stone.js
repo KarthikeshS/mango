ad6bb272c90b7ed65025413652d71e12463086c3
KarthikeshS <78033206+KarthikeshS@users.noreply.github.com>
2021-05-03 20:48:18 +0530

    Add files via upload

---
+++ js/stone.js
@@ -0,0 +1,28 @@
+class Stone{
+    constructor(x, y,r) {
+        var options = {
+            isStatic:false,
+            'restitution':0,
+            'friction':1,
+            'density':1.2
+        }
+        this.body = Bodies.circle(x, y, r/2, options);
+        this.r = r;
+       // this.height = height;
+       this.image = loadImage("images/stone.png");
+       
+        World.add(world, this.body);
+      }
+      display(){
+        //var angle = this.body.angle;
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+       // rotate(angle);
+        ellipseMode(RADIUS);
+        //ellipse( 0, 0, this.r, this.r);
+        imageMode(CENTER);
+        image(this.image, 0, 0,this.r*2, this.r*2);
+        pop();
+      }
+}
